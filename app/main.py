ec743c88da6d25670e74f43c186df78aeaded115
Antonius Prasetyo <antoniusp99@gmail.com>
2025-06-07 00:54:07 +0800

    Add UserProgress XP tracking

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
-from app.routes import focus_routes, focus_quest, focus_tracker
+from app.routes import focus_routes, focus_quest, focus_tracker, user_progress
 
 app = FastAPI()
 app.include_router(focus_routes.router)
 app.include_router(focus_quest.router)
 app.include_router(focus_tracker.router)
+app.include_router(user_progress.router)
