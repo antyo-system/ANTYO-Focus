1ed52e010549b741e71840e9db4cd7846e81e2d8
Antonius Prasetyo <antoniusp99@gmail.com>
2025-06-07 00:14:40 +0800

    Add FocusQuest module

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-from app.routes import focus_routes
+from app.routes import focus_routes, focus_quest
 
 app = FastAPI()
 app.include_router(focus_routes.router)
+app.include_router(focus_quest.router)
